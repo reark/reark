a58c4e59c17a0e0c4a52a74f49bddc40371f1d89
Timo Tuominen <timo.tuominen@gmail.com>
2014-06-13 09:09:48 +0100

    Switch to using CompositeSubscription

--- app/src/test/java/com/tehmou/rxbookapp/test/SubscriptionManagerTest.java
+++ app/src/test/java/com/tehmou/rxbookapp/test/SubscriptionManagerTest.java
@@ -1,8 +1,11 @@
-import com.tehmou.rxbookapp.utils.SubscriptionManager;
-
 import junit.framework.TestCase;
 
+import java.lang.Exception;
+import java.lang.Override;
+
 import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
 import static org.mockito.Mockito.*;
 
 
@@ -10,11 +13,11 @@ import static org.mockito.Mockito.*;
  * Created by tehmou on 12/25/13.
  */
 public class SubscriptionManagerTest extends TestCase {
-    private SubscriptionManager subscriptionManager;
+    private CompositeSubscription subscriptionManager;
 
     @Override
     protected void setUp() throws Exception {
-        subscriptionManager = new SubscriptionManager();
+        subscriptionManager = new CompositeSubscription();
     }
 
     public void testUnsubscribe() {
@@ -22,16 +25,32 @@ public class SubscriptionManagerTest extends TestCase {
         Subscription subscriptionB = mock(Subscription.class);
         subscriptionManager.add(subscriptionA);
         subscriptionManager.add(subscriptionB);
-        subscriptionManager.unsubscribeAll();
+        subscriptionManager.unsubscribe();
         verify(subscriptionA).unsubscribe();
         verify(subscriptionB).unsubscribe();
     }
 
     public void testUnsubscribeOnlyOnce() {
-        Subscription subscriptionA = mock(Subscription.class);
+        Subscription subscriptionA = new TestSubscription();
         subscriptionManager.add(subscriptionA);
         subscriptionManager.add(subscriptionA);
-        subscriptionManager.unsubscribeAll();
-        verify(subscriptionA).unsubscribe();
+        subscriptionManager.unsubscribe();
+    }
+
+    static class TestSubscription implements Subscription {
+        private boolean isSubscribed = true;
+
+        @Override
+        public void unsubscribe() {
+            if (!isSubscribed) {
+                fail("unsubscribed when not subscribed");
+            }
+            isSubscribed = false;
+        }
+
+        @Override
+        public boolean isUnsubscribed() {
+            return !isSubscribed;
+        }
     }
 }

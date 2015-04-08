b062ae266abda2f26f4e3eff0b9320a7e2ac89de
Timo Tuominen <timo.tuominen@gmail.com>
2015-04-08 13:57:23 +0100

    Fix DaggerGraph against updated snapshot

--- app/src/main/java/com/tehmou/rxbookapp/Graph.java
+++ app/src/main/java/com/tehmou/rxbookapp/Graph.java
@@ -19,7 +19,7 @@ public interface Graph {
     public final static class Initializer {
 
         public static Graph init() {
-            return Dagger_Graph.builder()
+            return DaggerGraph.builder()
                     .build();
         }
     }

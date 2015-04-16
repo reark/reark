893d46928a7abbb2515e3a2142fea10969b69129
Timo Tuominen <timo.tuominen@gmail.com>
2015-04-16 19:58:10 +0100

    Introduce fetchers

--- app/src/main/java/com/tehmou/rxbookapp/network/ServiceDataLayer.java
+++ app/src/main/java/com/tehmou/rxbookapp/network/ServiceDataLayer.java
@@ -6,12 +6,14 @@ import android.net.Uri;
 import android.util.Log;
 
 import com.tehmou.rxbookapp.data.DataLayerBase;
-import com.tehmou.rxbookapp.data.GitHubRepositorySearchStore;
-import com.tehmou.rxbookapp.data.GitHubRepositoryStore;
+import com.tehmou.rxbookapp.network.fetchers.GitHubRepositoryFetcher;
+import com.tehmou.rxbookapp.network.fetchers.GitHubRepositorySearchFetcher;
 import com.tehmou.rxbookapp.pojo.GitHubRepository;
 import com.tehmou.rxbookapp.pojo.GitHubRepositorySearch;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,79 +26,43 @@ import rx.schedulers.Schedulers;
  */
 public class ServiceDataLayer extends DataLayerBase {
     private static final String TAG = ServiceDataLayer.class.getSimpleName();
-    final private NetworkApi networkApi;
+    final private Collection<Fetcher> fetchers;
 
     public ServiceDataLayer(ContentResolver contentResolver) {
         super(contentResolver);
-        this.networkApi = new NetworkApi();
+        NetworkApi networkApi = new NetworkApi();
+        final Fetcher gitHubRepositoryFetcher = new GitHubRepositoryFetcher(
+                networkApi, gitHubRepositoryStore);
+        final Fetcher gitHubRepositorySearchFetcher = new GitHubRepositorySearchFetcher(
+                networkApi, gitHubRepositoryStore, gitHubRepositorySearchStore);
+        fetchers = Arrays.asList(
+                gitHubRepositoryFetcher,
+                gitHubRepositorySearchFetcher
+        );
     }
-
 
     public void processIntent(Intent intent) {
         final String contentUriString = intent.getStringExtra("contentUriString");
         if (contentUriString != null) {
-            final Uri uri = Uri.parse(contentUriString);
-            if (uri.equals(gitHubRepositoryStore.getContentUri())) {
-                final int repositoryId = intent.getIntExtra("id", -1);
-                if (repositoryId != -1) {
-                    fetchGitHubRepository(repositoryId);
-                } else {
-                    Log.e(TAG, "No repositoryId provided in the intent extras");
-                }
-            } else if (uri.equals(gitHubRepositorySearchStore.getContentUri())) {
-                final String searchString = intent.getStringExtra("searchString");
-                if (searchString != null) {
-                    fetchGitHubSearch(searchString);
-                } else {
-                    Log.e(TAG, "No searchString provided in the intent extras");
-                }
+            final Uri contentUri = Uri.parse(contentUriString);
+            Fetcher matchingFetcher = findFetcher(contentUri);
+            if (matchingFetcher != null) {
+                Log.v(TAG, "Fetcher found for " + contentUri);
+                matchingFetcher.fetch(intent);
             } else {
-                Log.e(TAG, "Unknown Uri " + uri);
+                Log.e(TAG, "Unknown Uri " + contentUri);
             }
         } else {
             Log.e(TAG, "No Uri defined");
         }
     }
 
-    private void fetchGitHubRepository(final int repositoryId) {
-        Log.d(TAG, "fetchGitHubRepository(" + repositoryId + ")");
-        Observable.<GitHubRepository>create(subscriber -> {
-            try {
-                GitHubRepository repository = networkApi.getRepository(repositoryId);
-                subscriber.onNext(repository);
-                subscriber.onCompleted();
-            } catch (Exception e) {
-                subscriber.onError(e);
+    private Fetcher findFetcher(Uri contentUri) {
+        for (Fetcher fetcher : fetchers) {
+            if (fetcher.getContentUri().equals(contentUri)) {
+                return fetcher;
             }
-        })
-                .subscribeOn(Schedulers.computation())
-                .subscribe(gitHubRepositoryStore::put,
-                        e -> Log.e(TAG, "Error fetching GitHub repository " + repositoryId, e));
-    }
-
-    private void fetchGitHubSearch(final String searchString) {
-        Log.d(TAG, "fetchGitHubSearch(" + searchString + ")");
-        Observable.<List<GitHubRepository>>create((subscriber) -> {
-            try {
-                Map<String, String> params = new HashMap<>();
-                params.put("q", searchString);
-                List<GitHubRepository> results = networkApi.search(params);
-                subscriber.onNext(results);
-                subscriber.onCompleted();
-            } catch (Exception e) {
-                subscriber.onError(e);
-            }
-        })
-                .subscribeOn(Schedulers.computation())
-                .map((repositories) -> {
-                    final List<Integer> repositoryIds = new ArrayList<>();
-                    for (GitHubRepository repository : repositories) {
-                        gitHubRepositoryStore.put(repository);
-                        repositoryIds.add(repository.getId());
-                    }
-                    return new GitHubRepositorySearch(searchString, repositoryIds);
-                })
-                .subscribe(gitHubRepositorySearchStore::put,
-                        e -> Log.e(TAG, "Error fetching GitHub repository search for '" + searchString + "'", e));
+        }
+        return null;
     }
 }
---
+++ app/src/main/java/com/tehmou/rxbookapp/network/fetchers/GitHubRepositoryFetcher.java
@@ -0,0 +1,60 @@
+package com.tehmou.rxbookapp.network.fetchers;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import com.tehmou.rxbookapp.data.GitHubRepositoryStore;
+import com.tehmou.rxbookapp.network.Fetcher;
+import com.tehmou.rxbookapp.network.NetworkApi;
+import com.tehmou.rxbookapp.pojo.GitHubRepository;
+
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by ttuo on 16/04/15.
+ */
+public class GitHubRepositoryFetcher implements Fetcher {
+    private static final String TAG = GitHubRepositoryFetcher.class.getSimpleName();
+
+    private final NetworkApi networkApi;
+    private final GitHubRepositoryStore gitHubRepositoryStore;
+
+    public GitHubRepositoryFetcher(NetworkApi networkApi,
+                                   GitHubRepositoryStore gitHubRepositoryStore) {
+        this.networkApi = networkApi;
+        this.gitHubRepositoryStore = gitHubRepositoryStore;
+    }
+
+    @Override
+    public void fetch(Intent intent) {
+        final int repositoryId = intent.getIntExtra("id", -1);
+        if (repositoryId != -1) {
+            fetchGitHubRepository(repositoryId);
+        } else {
+            Log.e(TAG, "No repositoryId provided in the intent extras");
+        }
+    }
+
+    private void fetchGitHubRepository(final int repositoryId) {
+        Log.d(TAG, "fetchGitHubRepository(" + repositoryId + ")");
+        Observable.<GitHubRepository>create(subscriber -> {
+            try {
+                GitHubRepository repository = networkApi.getRepository(repositoryId);
+                subscriber.onNext(repository);
+                subscriber.onCompleted();
+            } catch (Exception e) {
+                subscriber.onError(e);
+            }
+        })
+                .subscribeOn(Schedulers.computation())
+                .subscribe(gitHubRepositoryStore::put,
+                        e -> Log.e(TAG, "Error fetching GitHub repository " + repositoryId, e));
+    }
+
+    @Override
+    public Uri getContentUri() {
+        return gitHubRepositoryStore.getContentUri();
+    }
+}

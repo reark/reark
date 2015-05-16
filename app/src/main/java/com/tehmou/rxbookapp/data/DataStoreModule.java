d4c6e9881eb09d08c65d5daea206e4cfb62e3a5c
Pawel Polanski <pawel.polanski@futurice.com>
2015-05-16 20:37:57 +0200

    Injecting NetworkApi and Fetchers

--- app/src/main/java/com/tehmou/rxbookapp/data/DataStoreModule.java
+++ app/src/main/java/com/tehmou/rxbookapp/data/DataStoreModule.java
@@ -3,13 +3,16 @@ package com.tehmou.rxbookapp.data;
 import com.tehmou.rxbookapp.data.stores.GitHubRepositorySearchStore;
 import com.tehmou.rxbookapp.data.stores.GitHubRepositoryStore;
 import com.tehmou.rxbookapp.data.stores.NetworkRequestStatusStore;
+import com.tehmou.rxbookapp.data.stores.StoreModule;
 import com.tehmou.rxbookapp.data.stores.UserSettingsStore;
 import com.tehmou.rxbookapp.injections.ForApplication;
-import com.tehmou.rxbookapp.network.NetworkApi;
 import com.tehmou.rxbookapp.network.ServiceDataLayer;
+import com.tehmou.rxbookapp.network.fetchers.Fetcher;
+import com.tehmou.rxbookapp.network.fetchers.FetcherModule;
 
 import android.content.Context;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -18,7 +21,7 @@ import dagger.Provides;
 /**
  * Created by pt2121 on 2/20/15.
  */
-@Module
+@Module(includes = { FetcherModule.class, StoreModule.class })
 public final class DataStoreModule {
 
     @Provides
@@ -58,11 +61,16 @@ public final class DataStoreModule {
 
     @Provides
     @Singleton
-    public ServiceDataLayer provideServiceDataLayer(NetworkApi networkApi,
+    public ServiceDataLayer provideServiceDataLayer(@Named("gitHubRepository")Fetcher gitHubRepositoryFetcher,
+                                                    @Named("gitHubRepositorySearch") Fetcher gitHubRepositorySearchFetcher,
                                                     NetworkRequestStatusStore networkRequestStatusStore,
                                                     GitHubRepositoryStore gitHubRepositoryStore,
                                                     GitHubRepositorySearchStore gitHubRepositorySearchStore) {
-        return new ServiceDataLayer(networkApi, networkRequestStatusStore, gitHubRepositoryStore, gitHubRepositorySearchStore);
+        return new ServiceDataLayer(gitHubRepositoryFetcher,
+                                    gitHubRepositorySearchFetcher,
+                                    networkRequestStatusStore,
+                                    gitHubRepositoryStore,
+                                    gitHubRepositorySearchStore);
     }
 
 }
---
+++ app/src/main/java/com/tehmou/rxbookapp/network/NetworkModule.java
@@ -0,0 +1,20 @@
+package com.tehmou.rxbookapp.network;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Pawel Polanski on 5/16/15.
+ */
+@Module
+public final class NetworkModule {
+
+    @Provides
+    @Singleton
+    public NetworkApi provideNetworkApi() {
+        return new NetworkApi();
+    }
+
+}

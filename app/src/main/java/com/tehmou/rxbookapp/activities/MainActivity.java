708be9e91098cbfc29e0d1b921eab7284e987b87
Pawel Polanski <pawel.polanski@futurice.com>
2015-06-05 18:06:29 +0200

    Fix: Removed unused menu item

--- app/src/main/java/com/tehmou/rxbookapp/activities/MainActivity.java
+++ app/src/main/java/com/tehmou/rxbookapp/activities/MainActivity.java
@@ -37,26 +37,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     public void chooseRepository() {
         Log.d(TAG, "chooseRepository");
         Intent intent = new Intent(this, ChooseRepositoryActivity.class);

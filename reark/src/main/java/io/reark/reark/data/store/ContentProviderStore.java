9f7f87cad310d54af64866dfc1d363206d0aa191
Antti Poikela <antti.poikela@iki.fi>
2015-10-28 17:15:07 +0100

    Execute database operations in computation thread

--- reark/src/main/java/io/reark/reark/data/store/ContentProviderStore.java
+++ reark/src/main/java/io/reark/reark/data/store/ContentProviderStore.java
@@ -10,11 +10,15 @@ import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reark.reark.utils.Preconditions;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by ttuo on 26/04/15.
@@ -28,10 +32,32 @@ public abstract class ContentProviderStore<T> {
     @NonNull
     private final ContentObserver contentObserver = getContentObserver();
 
+    protected final PublishSubject<Pair<T, Uri>> updateSubject = PublishSubject.create();
+
     public ContentProviderStore(ContentResolver contentResolver) {
         this.contentResolver = contentResolver;
         this.contentResolver.registerContentObserver(
                 getContentUri(), true, contentObserver);
+
+        subscribe();
+    }
+
+    private void subscribe() {
+        updateSubject
+                .onBackpressureBuffer()
+                .observeOn(Schedulers.computation())
+                .subscribe(pair -> {
+                    ContentValues values = getContentValuesForItem(pair.first);
+                    Log.v(TAG, "insertOrUpdate to " + pair.second);
+                    Log.v(TAG, "values(" + values + ")");
+
+                    if (contentResolver.update(pair.second, values, null, null) == 0) {
+                        final Uri resultUri = contentResolver.insert(pair.second, values);
+                        Log.v(TAG, "Inserted at " + resultUri);
+                    } else {
+                        Log.v(TAG, "Updated at " + pair.second);
+                    }
+                });
     }
 
     @NonNull
@@ -44,23 +70,14 @@ public abstract class ContentProviderStore<T> {
     protected void insertOrUpdate(T item, Uri uri) {
         Preconditions.checkNotNull(item, "Item to be inserted cannot be null");
 
-        Log.v(TAG, "insertOrUpdate to " + uri);
-        ContentValues values = getContentValuesForItem(item);
-        Log.v(TAG, "values(" + values + ")");
-        if (contentResolver.update(uri, values, null, null) == 0) {
-            final Uri resultUri = contentResolver.insert(uri, values);
-            Log.v(TAG, "Inserted at " + resultUri);
-        } else {
-            Log.v(TAG, "Updated at " + uri);
-        }
+        updateSubject.onNext(new Pair<>(item, uri));
     }
 
     @NonNull
     protected List<T> queryList(Uri uri) {
-        Preconditions.checkNotNull(uri, "Uri cannot be null");
+        Preconditions.checkNotNull(uri, "Uri cannot be null.");
 
-        Cursor cursor = contentResolver.query(uri,
-                getProjection(), null, null, null);
+        Cursor cursor = contentResolver.query(uri, getProjection(), null, null, null);
         List<T> list = new ArrayList<>();
 
         if (cursor != null) {
@@ -78,7 +95,7 @@ public abstract class ContentProviderStore<T> {
         return list;
     }
 
-    @Nullable
+    @NonNull
     protected T queryOne(Uri uri) {
         final List<T> queryResults = queryList(uri);
 

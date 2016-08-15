e8dcd4db0e8ca958495e1709b92a8048b6780056
tehmou <timo.tuominen@gmail.com>
2016-08-15 22:52:18 +0100

    Clean up unused logs and fix licenses

--- appbasic/src/main/java/io/reark/rxgithubapp/basic/data/DataLayer.java
+++ appbasic/src/main/java/io/reark/rxgithubapp/basic/data/DataLayer.java
@@ -1,3 +1,28 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2013-2016 reark project contributors
+ *
+ * https://github.com/reark/reark/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package io.reark.rxgithubapp.basic.data;
 
 import android.content.Intent;
@@ -13,9 +38,6 @@ import io.reark.rxgithubapp.basic.data.stores.UserSettingsStore;
 import io.reark.rxgithubapp.shared.data.ClientDataLayerBase;
 import io.reark.rxgithubapp.shared.network.GitHubService;
 
-/**
- * Created by ttuo on 27/06/16.
- */
 public class DataLayer extends ClientDataLayerBase {
     private static final String TAG = DataLayer.class.getSimpleName();
 
--- appbasic/src/main/java/io/reark/rxgithubapp/basic/data/stores/GitHubRepositoryStore.java
+++ appbasic/src/main/java/io/reark/rxgithubapp/basic/data/stores/GitHubRepositoryStore.java
@@ -1,16 +1,37 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2013-2016 reark project contributors
+ *
+ * https://github.com/reark/reark/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package io.reark.rxgithubapp.basic.data.stores;
 
 import io.reark.reark.data.stores.MemoryStore;
 import io.reark.reark.utils.Log;
 import io.reark.rxgithubapp.shared.pojo.GitHubRepository;
 
-/**
- * Created by ttuo on 27/06/16.
- */
 public class GitHubRepositoryStore extends MemoryStore<Integer, GitHubRepository> {
     public GitHubRepositoryStore() {
         // Use a custom merge function for put values.
         super(GitHubRepository::getId, (v1, v2) -> new GitHubRepository(v1).overwrite(v2));
-        Log.d("lolg", "create");
     }
 }
